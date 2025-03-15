7d3846a15526a5e3c782f81cdc4723591533c65b
guillermoll <guillermo.jobs@hotmail.com>
2025-03-15 13:31:15 +0000

    inicio css angular

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', loadComponent: async () => await import('./users/users.component').then(c => c.UsersComponent) }
+
+]
