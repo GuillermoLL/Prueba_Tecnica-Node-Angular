ff7586b1e8a2c13ac42f5b13043c84be73ed66fb
guillermoll <guillermo.jobs@hotmail.com>
2025-03-15 12:49:16 +0000

    servidor node completado

--- node-app/users/user.controller.js
+++ node-app/users/user.controller.js
@@ -1,24 +1,93 @@
-import data from './data.json' assert {type: 'json'}
+// import data from './data.json'
+import url from 'node:url'
+import { readFile } from 'node:fs/promises'
+const file = await readFile('./users/data.json', 'utf-8')
+const data = JSON.parse(file)
 
-export function UserController (request, response) {
-  const { method, url } = request
+export async function UserController (request, response) {
+  const { method } = request
+  const { pathname, query } = url.parse(request.url, true)
 
-  if (method === 'POST' && url === '/users') {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(data))
-    return
+  if (method === 'POST' && pathname === '/users') {
+    let bodyChunks = ''
+    await request
+      .on('data', chunk => {
+        bodyChunks += chunk.toString()
+      })
+      .on('end', () => {
+        const body = JSON.parse(bodyChunks)
+
+        if (!validateEntry(body)) {
+          response.writeHead(400, { 'Content-Type': 'application/json; charset=utf-8' })
+          response.end(JSON.stringify({
+            error: 'Falta alguna propiedad'
+          }))
+          return
+        }
+
+        response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
+        response.end(JSON.stringify(paginateData(body)))
+      })
   }
 
   // Me parece una mala practica usar el metodo POST para filtrar
-  // asi que hice lo mismo pero con el metodo GET
-  if (method === 'GET' && url === '/users') {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(data))
+  // asi que hice lo mismo pero con el metodo GET utilizando los parametros de query
+  if (method === 'GET' && pathname === '/users') {
+    if (!validateEntry(query)) {
+      response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
+      response.end(JSON.stringify(data))
+      return
+    }
+    response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
+    response.end(JSON.stringify(paginateData(query)))
     return
   }
 
-  response.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' })
-  response.write('<h1>Page not found</h1>')
-  response.end()
-  return
+  if (method === 'GET') {
+    response.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' })
+    response.write('<h1>Page not found</h1>')
+    response.end()
+  }
+}
+
+function paginateData (data) {
+  let { filter, page, per_page: perPage } = data
+
+  page = parseInt(page)
+  perPage = parseInt(perPage)
+  filter = filter.toLowerCase()
+
+  const usersBlock = (page - 1) * perPage
+
+  const filteredData = filterData({ filter, page, perPage })
+  const pages = Math.ceil(filteredData.length / perPage)
+  const slicedData = filteredData.splice(usersBlock, usersBlock + perPage)
+
+  return {
+    _metadata: {
+      total: data.length,
+      pages,
+      page,
+      per_page: perPage,
+      filter
+    },
+    users: slicedData
+  }
+}
+
+function filterData ({ filter, page, perPage }) {
+  const filteredData = data.filter((elm) =>
+    elm.name.toLowerCase().includes(filter) ||
+    elm.surname1.toLowerCase().includes(filter) ||
+    elm.surname2.toLowerCase().includes(filter) ||
+    elm.email.toLowerCase().includes(filter)
+  )
+
+  return filteredData
+}
+
+function validateEntry (objectToValidate) {
+  if (objectToValidate.filter && objectToValidate.page && objectToValidate.per_page) { return true }
+
+  return false
 }
